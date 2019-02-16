9b5841c2fea29ec681c5bc5eb75003e843dd3261
EsperantoDeparture <juanpablordmsx2@gmail.com>
2019-02-15 21:03:43 -0500

    Material dependencies and component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconRegistry, MatToolbarModule, MatButtonModule, MatTooltipModule, MatIconModule } from '@angular/material';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'home',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/home.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'navigate_before',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/navigate_before.svg')
+    );
+    iconRegistry.addSvgIcon(
+      'navigate_next',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/navigate_next.svg')
+    );
+  }
+}
